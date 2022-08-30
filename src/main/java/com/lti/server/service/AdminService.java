1335b882a8828daf0a98b1c66c69fc194e8488b3
antara <iamantaraslg@gmail.com>
2022-08-30 12:15:35 +0530

    Admin Service Done

--- src/main/java/com/lti/server/service/AdminService.java
+++ src/main/java/com/lti/server/service/AdminService.java
@@ -1,5 +1,24 @@
 package com.lti.server.service;
 
-public interface AdminService {
+import java.util.List;
 
+import com.lti.server.entity.Admin;
+import com.lti.server.entity.Product;
+import com.lti.server.entity.User;
+
+public interface AdminService {
+	
+	public List<Admin> getByAdminId(long adminId);
+
+	public List<User> getUserdetails();
+
+	public List<User> findUser(int userId);
+
+	public boolean deleteUser(int userId);
+
+	public String addProduct(Product product);
+
+	public boolean deleteProduct(int prdId);
+
+	public String updateCardStatus(int userId);
 }
--- src/main/java/com/lti/server/service/AdminServiceImpl.java
+++ src/main/java/com/lti/server/service/AdminServiceImpl.java
@@ -1,5 +1,60 @@
 package com.lti.server.service;
 
-public class AdminServiceImpl {
+import java.util.List;
 
+import com.lti.server.entity.Admin;
+import com.lti.server.entity.Product;
+import com.lti.server.entity.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AdminServiceImpl {
+	
+	@Autowired
+	AdminDao dao;
+
+	@Override
+	public List<Admin> getByAdminId(long adminId) {
+		System.out.println("service layer called");
+		return dao.getByAdminId(adminId);
+	}
+
+	@Override
+	public List<User> getUserdetails() {
+		return dao.getUserdetails();
+	}
+
+	@Override
+	public List<User> findUser(int userId) {
+		System.out.println("service layer called");
+		return dao.findUser(userId);
+	}
+
+	@Override
+	public boolean deleteUser(int userId) {
+		dao.deleteUser(userId);
+		return true;
+	}
+
+	@Override
+	public String addProduct(Product product) {
+		return dao.addProduct(product);
+
+	}
+
+	@Override
+	public boolean deleteProduct(int prdId) {
+		dao.deleteProduct(prdId);
+		return true;
+	}
+
+	@Override
+	public String updateCardStatus(int userId) {
+		dao.updateCardStatus(userId);
+		return "verified";
+	}
 }
