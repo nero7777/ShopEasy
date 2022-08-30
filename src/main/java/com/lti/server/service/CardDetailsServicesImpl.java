f2452502b55a1dbc0930144de2eecc5e961146e1
Swati1289 <hegdeswati30@gmail.com>
2022-08-30 13:28:52 +0530

    Card Details Dao Update

--- src/main/java/com/lti/server/service/CardDetailsServicesImpl.java
+++ src/main/java/com/lti/server/service/CardDetailsServicesImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lti.server.dao.CardDetailsDao;
 import com.lti.server.entity.CardDetails;
 
 @Service
